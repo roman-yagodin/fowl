bc9664c81135d08ee70b84cf687ec406bfeebe68
roman-yagodin <roman.yagodin@gmail.com>
2023-04-05 17:31:38 +0300

    Initial commit

---
+++ Program.fs
@@ -0,0 +1,17 @@
+﻿open System.Reflection
+
+let asm = Assembly.LoadFrom "./samples/Xwt.dll"
+
+printfn "\n# Referenced assemblie:"
+
+let refAsms = asm.GetReferencedAssemblies()
+for i in 0 .. refAsms.Length - 1 do
+    let refAsm = refAsms[i]
+    printfn "%s" refAsm.FullName
+
+printfn "\n# Exported types:"
+
+let exportedTypes = asm.GetExportedTypes()
+for i in 0 .. exportedTypes.Length - 1 do
+    let exportedType = exportedTypes[i]
+    printfn "%s" exportedType.FullName
